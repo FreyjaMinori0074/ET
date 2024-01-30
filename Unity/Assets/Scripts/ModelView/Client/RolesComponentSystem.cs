692fb3219b62e59a72c366e4e332264208126a0a
Takasaki You <zzwzcd35@icloud.com>
2024-01-31 00:34:43 +0800

    提交一个Component和System

---
+++ Unity/Assets/Scripts/ModelView/Client/RolesComponentSystem.cs
@@ -0,0 +1,18 @@
+﻿namespace ET.Client
+{
+    [EntitySystemOf(typeof(RolesComponent))]
+    [FriendOf(typeof(RolesComponent))]
+    public static partial class RolesComponentSystem
+    {
+        [EntitySystem]
+        private static void Awake(this RolesComponent self)
+        {
+            
+        }
+
+        public static void Invoke(this RolesComponent self)
+        {
+            Log.Debug($"{self.MaxCount.ToString()}");
+        }
+    }
+}
